5ae5f662a962b6616300385c94e1fb975a64939e
noultrabody <a85antony@gmail.com>
2017-11-27 16:50:02 +0800

    upload v1.0

--- 20171122/upload.php
+++ 20171122/upload.php
@@ -1,15 +1,33 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>post</title>
+</head>
+<body>
+    <form action="#" method="post" enctype="multipart/form-data">
+        height: <input type="text" name="height"> m<br>
+        weight: <input type="text" name="weight"> kg<br>
+        <input type="file" name="file" id="file"><br>
+        <input type="submit" name="submit" value="提交">
+    </form>
+    </body>
+</html>
 <?php
 extract ($_POST);
 if(empty($height)||empty($weight)==true){
-    echo "please type in all information";
+    echo "please type in all information<br>";
 }else{
     echo "height=$height<br>";
     echo "weight=$weight<br>";
     echo "BMI=".$weight/$height/$height."<br>";
 }
-if(empty($_FILES)==true){
+if(empty($_FILES)===true){
     echo "empty";
-}elseif($_FILES["file"]["type"]==""){
-
+}elseif(strpos($_FILES["file"]["type"],"image/")!==false){
+    $filename=iconv('utf-8','big5', $_FILES["file"]["name"]);
+    move_uploaded_file($_FILES["file"]["tmp_name"],"upload/".$filename);
+    echo '<img src="upload/'.$filename.'"/>';
+}else{
+    echo "wrong file type";
 }
 ?>
